3752eb273d19cc1b6ce35af3da88750bdf376261
hsmtkk <hsmtkk@gmail.com>
2023-04-13 16:20:17 +0900

    add log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 FRONTEND_PORT=$PORT
-uvicorn backend:app --reload --host 0.0.0.0 --port $BACKEND_PORT &
+
+echo "frontend port: ${FRONTEND_PORT}"
+echo "backend port: ${BACKEND_PORT}"
+
+uvicorn backend:app --host 0.0.0.0 --port $BACKEND_PORT &
 streamlit run frontend.py --server.address 0.0.0.0 --server.port $FRONTEND_PORT
