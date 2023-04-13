9bd567610e80bb32cc0e8d95b8eae2e2db13118b
hsmtkk <hsmtkk@gmail.com>
2023-04-13 15:59:01 +0900

    add frontend

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-uvicorn main:app --reload --host 0.0.0.0 --port $PORT
+FRONTEND_PORT=$PORT
+uvicorn backend:app --reload --host 0.0.0.0 --port $BACKEND_PORT &
+streamlit run frontend.py --server.address 0.0.0.0 --server.port $FRONTEND_PORT
